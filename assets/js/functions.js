53468726f8e7176e4a1eafd3ae6523a90254dc8b
Ineskay <victordivi98@gmail.com>
2019-10-04 10:33:56 +0200

    Add little more logic

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,27 +1,40 @@
 function onSubmit() {
+    const tech = document.getElementById('Tecnologia').checked;
     document.getElementById('separator').hidden = true;
     document.getElementById('recomenacions').hidden = false;
     const duracio = document.getElementById('estudis').selectedIndex;
-    switch (duracio) {
-        case 0:
-            document.getElementById('bootcamp').hidden = false;
-            document.getElementById('superior').hidden = true;
-            document.getElementById('grau').hidden = true;
-            break;
-        case 1:
-            document.getElementById('bootcamp').hidden = true;
-            document.getElementById('superior').hidden = false;
-            document.getElementById('grau').hidden = true;
-            break;
-        case 2:
-            document.getElementById('bootcamp').hidden = true;
-            document.getElementById('superior').hidden = true;
-            document.getElementById('grau').hidden = false;
-            break;
+    if (tech) {
+        document.getElementById('noRecomendations').hidden = true;
+        switch (duracio) {
+            case 0:
+                document.getElementById('bootcamp').hidden = false;
+                document.getElementById('superior').hidden = true;
+                document.getElementById('grau').hidden = true;
+                break;
+            case 1:
+                document.getElementById('bootcamp').hidden = true;
+                document.getElementById('superior').hidden = false;
+                document.getElementById('grau').hidden = true;
+                break;
+            case 2:
+                document.getElementById('bootcamp').hidden = true;
+                document.getElementById('superior').hidden = true;
+                document.getElementById('grau').hidden = false;
+                break;
+        }
+    } else {
+        document.getElementById('bootcamp').hidden = true;
+        document.getElementById('superior').hidden = true;
+        document.getElementById('grau').hidden = true;
+        document.getElementById('noRecomendations').hidden = false;
     }
 }
 
 
 function onLoad2() {
+    document.getElementById('bootcamp').hidden = true;
+    document.getElementById('superior').hidden = true;
+    document.getElementById('grau').hidden = true;
+    document.getElementById('noRecomendations').hidden = true;
     document.getElementById('recomenacions').hidden = true;
 }
